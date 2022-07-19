fb0707fe3834bb0cd5a1d99f28141dde6108af6b
Ahmed <Ahmed@DESKTOP-9N0OBRE>
2022-07-20 00:58:58 +0300

    Book, Category,Language, Sector rerpostories had written.

--- DataAccess/Implementation/PostgreSql/SqlBookRepository.cs
+++ DataAccess/Implementation/PostgreSql/SqlBookRepository.cs
@@ -1,5 +1,7 @@
-﻿using Library.DataAccess.Abstraction;
+﻿using Library.Core.Extensions;
+using Library.DataAccess.Abstraction;
 using Library.Entities.Concrete;
+using Npgsql;
 
 namespace Library.DataAccess.Implementation.PostgreSql
 {
@@ -11,27 +13,81 @@ namespace Library.DataAccess.Implementation.PostgreSql
 
         public bool Add(Book value)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Insert Into Books(Name,CategoryId,OriginalLanguageId,LastModified,IsDeleted)" +
+                               " Values(@name,@categoryId,@originalCategoryId,@lastModified,@isDeleted)";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString, connection);
+            command.Parameters.AddWithValue("@name", value.Name);
+            command.Parameters.AddWithValue("@categoryId", value.CategoryId);
+            command.Parameters.AddWithValue("@originalLanguageId", value.OriginalLangaugeId);
+            command.Parameters.AddWithValue("@lastModified", value.LastModified);
+            command.Parameters.AddWithValue("@isDeleted", false);
+            return 1 == command.ExecuteNonQuery();
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Delete From Books Where Id=@id";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString,connection);
+            command.Parameters.AddWithValue("@id", id);
+            return 1 == command.ExecuteNonQuery();
         }
 
         public Book Get(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Select * From Books Where Id=@id";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString,connection);
+            var reader = command.ExecuteReader();
+            if (reader.Read())
+                return ReadBook(reader);
+            return null;
         }
 
         public List<Book> GetAll()
         {
-            throw new NotImplementedException();
+            List<Book> books = new List<Book>();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Select * From Books";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString, connection);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+                 books.Add(ReadBook(reader));
+            return books;
         }
 
         public bool Update(Book value)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString =
+                "Update Books Set Name=@name,CategoryId=@categoryId,OriginalLanguageId=@originalLanguageId,LastModified=@lastModified,IsDeleted=@isDeleted Where Id=@id";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString, connection);
+            command.Parameters.AddWithValue("@id", value.Id);
+            command.Parameters.AddWithValue("@name", value.Name);
+            command.Parameters.AddWithValue("@categoryId", value.CategoryId);
+            command.Parameters.AddWithValue("@originalLanguageId", value.OriginalLangaugeId);
+            command.Parameters.AddWithValue("@lastModified", value.LastModified);
+            command.Parameters.AddWithValue("@isDeleted", value.IsDeleted);
+            return 1 == command.ExecuteNonQuery();
+        }
+
+        private Book ReadBook(NpgsqlDataReader reader)
+        {
+            return new Book
+            {
+                Id = reader.Get<int>(nameof(Book.Id)),
+                CategoryId = reader.Get<int>(nameof(Book.CategoryId)),
+                OriginalLangaugeId = reader.Get<int>(nameof(Book.OriginalLangaugeId)),
+                IsDeleted = reader.Get<bool>(nameof(Book.IsDeleted)),
+                LastModified = reader.Get<DateTime>(nameof(Book.LastModified)),
+                Name = reader.Get<string>(nameof(Book.Name))
+            };
         }
     }
 }
--- DataAccess/Implementation/PostgreSql/SqlSectorRepository.cs
+++ DataAccess/Implementation/PostgreSql/SqlSectorRepository.cs
@@ -1,5 +1,7 @@
-﻿using Library.DataAccess.Abstraction;
+﻿using Library.Core.Extensions;
+using Library.DataAccess.Abstraction;
 using Library.Entities.Concrete;
+using Npgsql;
 
 namespace Library.DataAccess.Implementation.PostgreSql
 {
@@ -11,27 +13,68 @@ namespace Library.DataAccess.Implementation.PostgreSql
 
         public bool Add(Sector value)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Insert Into Sectors(Name) Values(@name)";
+            using NpgsqlCommand cmd = new NpgsqlCommand(cmdString, connection);
+            cmd.Parameters.AddWithValue("@name", value.Name);
+            return 1 == cmd.ExecuteNonQuery();
+
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Delete From Sectors Where Id = @id";
+            NpgsqlCommand cmd = new NpgsqlCommand(cmdString, connection);
+            cmd.Parameters.AddWithValue("@id", id);
+            return 1 == cmd.ExecuteNonQuery();
         }
 
         public Sector Get(int id)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Select * From Sectors Where Id=@id";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString, connection);
+            var reader = command.ExecuteReader();
+            if (reader.Read())
+                return ReadSector(reader);
+            return null;
         }
 
         public List<Sector> GetAll()
         {
-            throw new NotImplementedException();
+            List<Sector> sectors = new List<Sector>();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Select * From Sectors";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString, connection);
+            var reader = command.ExecuteReader();
+            while (reader.Read()) 
+                sectors.Add(ReadSector(reader));
+            return sectors;
         }
 
         public bool Update(Sector value)
         {
-            throw new NotImplementedException();
+            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            string cmdString = "Update Sectors Set Name = @name where Id = @id";
+            using NpgsqlCommand command = new NpgsqlCommand(cmdString, connection);
+            command.Parameters.AddWithValue("@name", value.Name);
+            command.Parameters.AddWithValue("@id", value.Id);
+            return 1==command.ExecuteNonQuery();
+        }
+
+        private Sector ReadSector(NpgsqlDataReader reader)
+        {
+            return new Sector
+            {
+                Id = reader.Get<int>(nameof(Sector.Name)),
+                Name = reader.Get<string>(nameof(Sector.Name))
+            };
         }
     }
 }
