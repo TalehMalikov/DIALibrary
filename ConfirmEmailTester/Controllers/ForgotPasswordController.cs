7757c5ff2d048bde64b2435286d366bd3c1d8aa9
Taleh Malikov <102857634+TalehMalikov@users.noreply.github.com>
2022-09-01 12:55:02 +0400

    .

---
+++ ConfirmEmailTester/Controllers/ForgotPasswordController.cs
@@ -0,0 +1,13 @@
+﻿using ConfirmEmailTester.Models;
+using ConfirmEmailTester.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebMatrix.WebData;
+
+namespace ConfirmEmailTester.Controllers
+{
+    public class ForgotPasswordController : Controller
+    {
+       
+    }
+}
---
+++ ConfirmEmailTester/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConfirmEmailTester.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
