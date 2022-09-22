5adb65fa39c0928fe79c6b8d5ec6614d30b93964
Taleh Malikov <102857634+TalehMalikov@users.noreply.github.com>
2022-09-22 09:13:27 +0400

    Adding files and photos

--- Library.WebUI/Systems/SystemDefaults.cs
+++ Library.WebUI/Systems/SystemDefaults.cs
@@ -2,10 +2,16 @@
 {
     public class SystemDefaults
     {
-        public const string DefaultPhotoPath = @"C:\DIALibrary\BookPhotos\";
+        #region Original
+        /*public const string DefaultPhotoPath = @"C:\DIALibrary\BookPhotos\";
         public const string DefaultFilePath = @"C:\DIALibrary\BookFiles\";
         public const string DefaultUrlForQRCode = "https://localhost:7058/File/ShowFileInfo?guid=";
-        public const string DefaultQRCodePhotoPath = @"C:\DIALibrary\QRCodeImages\";
+        public const string DefaultQRCodePhotoPath = @"C:\DIALibrary\QRCodeImages\";*/
+        #endregion
 
+        public const string DefaultPhotoPath = @"~\wwwroot\images\FakeBookPhotos\";
+        public const string DefaultFilePath = @"~\wwwroot\images\FakeBookFiles\";
+        //public const string DefaultUrlForQRCode = "https://localhost:7058/File/ShowFileInfo?guid=";
+        //public const string DefaultQRCodePhotoPath = @"C:\DIALibrary\QRCodeImages\";
     }
 }
