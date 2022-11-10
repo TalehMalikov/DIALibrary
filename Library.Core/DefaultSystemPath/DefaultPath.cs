c13cd98db7ea0d3409a35c31b39f3fe1698ce64a
Taleh Malikov <102857634+TalehMalikov@users.noreply.github.com>
2022-11-10 11:08:55 +0400

    .

--- Library.Core/DefaultSystemPath/DefaultPath.cs
+++ Library.Core/DefaultSystemPath/DefaultPath.cs
@@ -3,14 +3,15 @@
     public class DefaultPath
     {
         #region Original
-        public const string OriginalDefaultPhotoPath = @"C:\DIALibrary\BookPhotos\";
-        public const string OriginalDefaultFilePath = @"C:\DIALibrary\BookFiles\";
+        public const string OriginalDefaultPhotoPath = @"C:\wwwroot\DIALibrary\BookPhotos\";
+        public const string OriginalDefaultFilePath = @"C:\wwwroot\DIALibrary\BookFiles\";
+        public const string OriginalDefaultActivityPhotoPath = @"C:\wwwroot\DIALibrary\ActivityPhoto\";
         public const string OriginalDefaultUrlForQRCode = "https://localhost:7058/File/ShowFileInfo?guid=";
         public const string OriginalDefaultQRCodePhotoPath = @"C:\DIALibrary\QRCodeImages\";
         #endregion
 
-        public const string DefaultPhotoPath = "wwwroot/images/FakeBookPhotos/";
-        public const string DefaultFilePath = "wwwroot/images/FakeBookFiles/";
+        //public const string DefaultPhotoPath = "wwwroot/images/FakeBookPhotos/";
+        //public const string DefaultFilePath = "wwwroot/images/FakeBookFiles/";
         //public const string DefaultUrlForQRCode = "https://localhost:7058/File/ShowFileInfo?guid=";
         //public const string DefaultQRCodePhotoPath = @"C:\DIALibrary\QRCodeImages\";
     }
