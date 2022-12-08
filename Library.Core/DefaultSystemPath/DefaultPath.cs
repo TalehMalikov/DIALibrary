9d384172fd82ed8242cb328f208ab30f7ca3454d
Admin <Admin@DESKTOP-ND6HTM6>
2022-12-08 11:49:20 +0400

    .

--- Library.Core/DefaultSystemPath/DefaultPath.cs
+++ Library.Core/DefaultSystemPath/DefaultPath.cs
@@ -5,8 +5,6 @@
         public const string OriginalDefaultPhotoPath = @"C:\WWW\Files\BookPhotos\";
         public const string OriginalDefaultFilePath = @"C:\WWW\Files\BookFiles\";
         public const string OriginalDefaultActivityPhotoPath = @"C:\WWW\Files\ActivityPhoto\";
-        public const string OriginalDefaultUrlForQRCode = "https://lib.dia.edu.az/File/ShowFileInfo?guid=";
-        public const string OriginalDefaultQRCodePhotoPath = @"C:\WWW\Files\QRCodeImages\";
         public const string OriginalDefaultExternalSourcePath = @"C:\WWW\Files\ExternalSourcePhotos\";
     }
 }
